9a00499378194ef401e5acd45d392e9c36ba2678
Daniel Wydler <daniel@wydler.eu>
2026-05-16 14:16:30 +0200

    Add default labels and update options to standalone runner configuration

--- action.sh
+++ action.sh
@@ -376,6 +376,7 @@ export MY_RUNNER_TARGET
 # Setup runner start method. 
 # https://docs.github.com/en/actions/how-tos/manage-runners/self-hosted-runners/configure-the-application
 if [[ "$INPUT_RUNNER_START_METHOD" == "standalone" ]]; then
+	yq -i '.runcmd[-1] += " --no-default-labels --disableupdate"' cloud-init.template.yml && \
 	# shellcheck disable=SC2016
 	yq -i '.runcmd += ["$MY_RUNNER_DIR/run.sh"]' cloud-init.template.yml && \
 	echo "Set run method 'standalone' to cloud-init.template.yml."
