568e2305811af910a5d3a30c5e2f996091f8f4ec
Blackmoon <dwydler@users.noreply.github.com>
2026-05-16 14:12:56 +0200

    Add runner start method configuration (#21)

--- action.sh
+++ action.sh
@@ -373,6 +373,19 @@ export MY_RUNNER_DIR
 export MY_RUNNER_VERSION
 export MY_RUNNER_TARGET
 
+# Setup runner start method. 
+# https://docs.github.com/en/actions/how-tos/manage-runners/self-hosted-runners/configure-the-application
+if [[ "$INPUT_RUNNER_START_METHOD" == "standalone" ]]; then
+	# shellcheck disable=SC2016
+	yq -i '.runcmd += ["$MY_RUNNER_DIR/run.sh"]' cloud-init.template.yml && \
+	echo "Set run method 'standalone' to cloud-init.template.yml."
+elif [[ "$INPUT_RUNNER_START_METHOD" == "systemd" ]]; then
+	#https://docs.github.com/en/actions/how-tos/manage-runners/self-hosted-runners/configure-the-application
+	# shellcheck disable=SC2016
+	yq -i '.runcmd += ["$MY_RUNNER_DIR/svc.sh install $(id -nu)", "$MY_RUNNER_DIR/svc.sh start"]' cloud-init.template.yml && \
+	echo "Set run method 'systemd' to cloud-init.template.yml."
+fi
+
 # Substitute environment variables in the cloud-init template and create the final cloud-init configuration
 if [[ ! -f "cloud-init.template.yml" ]]; then
 	exit_with_failure "cloud-init.template.yml not found!"
@@ -433,7 +446,7 @@ fi
 MAX_RETRIES=$MY_CREATE_WAIT
 RETRY_COUNT=0
 while [[ $RETRY_COUNT -lt $MAX_RETRIES ]]; do
-	echo "Create Server..."
+	echo "Create Server $MY_NAME..."
 	if curl \
 	-X POST \
 	--fail-with-body \
